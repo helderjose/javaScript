13a087813c04f87af2fd44230e15d4079e1c8e52
Helder Jose Melo <helder.goncalves@accurate.com.br>
2020-02-18 14:41:32 -0300

    import export

---
+++ exemplos/import/helpers/Constants.js
@@ -0,0 +1,8 @@
+const routerUrlBase = "/vue-router-wcp";
+
+const routerPaths = {
+  MOVIE_SELECTION: `${routerUrlBase}/movie-selection`,
+  FOOD_SELECTION: `${routerUrlBase}/food-selection`,
+}
+
+export { routerUrlBase, routerPaths }
---
+++ exemplos/import/test-import.js
@@ -0,0 +1,9 @@
+/*
+https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
+
+obs: no projeto com vue funcionou.
+*/
+
+import { routerPaths } from './helpers/Constants.js'
+
+console.log(routerPaths.FOOD_SELECTION)
